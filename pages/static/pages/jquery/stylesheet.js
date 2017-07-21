c2121b10da81bcd63b3f6c13390527d368cc89f4
monroeloop <mike.loop@hotmail.com>
2017-07-20 21:16:28 -0700

    added ability to identity selectable jquery

--- pages/static/pages/jquery/stylesheet.js
+++ pages/static/pages/jquery/stylesheet.js
@@ -3,6 +3,8 @@
  */
 
 $(document).ready(function () {
+    // ready func
+
     $(".menubutton").click(function () {
         $("#bar1").slideDown("750");
         $("#bar2").fadeToggle("500");
@@ -11,60 +13,92 @@ $(document).ready(function () {
         $("#panel2").slideToggle("750");
         $("#panel3").slideToggle("750");
     });
+
+
+    // $(document).ready(function() {
+    //     $( "#dialog" ).dialog({
+    //       autoOpen: false,
+    //       maxWidth:800,
+    //       maxHeight: 2000,
+    //       width: 500,
+    //       height: 700,
+    //       resizable: true,
+    //       stack: true,
+    //       show: {
+    //         effect: "blind",
+    //         duration: 800
+    //       },
+    //       hide: {
+    //         effect: "fade",
+    //         duration: 800
+    //       }
+    //     });
+    //
+    //     $( "td[class=mon], td[class=tue], td[class=wed]," +
+    //         "td[class=thu], td[class=fri], td[class=sat], td[class=sun]" ).on( "click", function() {
+    //       $( "#dialog" ).dialog( "open" );
+    //     });
+    //   });
+
+    $("#columnthree").selectable({
+        selected: function (event, ui) {
+            console.log($(ui.selected).data('time'));
+        }
+    });
+
+    function createChildDialog() {
+        $('#childDialog').dialog({
+            autoOpen: false,
+            maxWidth: 500,
+            maxHeight: 600,
+            height: 500,
+            width: 400,
+            resizable: true,
+            modal: false
+        });
+    }
+
+    // $(".rowthree").on("click", function () {
+    //     $("#childDialog").dialog("open");
+    // });
+
+    function createParentDialog() {
+
+        $('#dialog').dialog({
+            autoOpen: false,
+            maxWidth: 800,
+            maxHeight: 2000,
+            width: 500,
+            height: 700,
+            resizable: true,
+            stack: true,
+            show: {
+                effect: "blind",
+                duration: 800
+            },
+            hide: {
+                effect: "fade",
+                duration: 800
+            },
+                buttons: {
+                    "Close": function () {
+                        $(this).dialog("close");
+                    },
+                    "Select Times": function () {
+                        createChildDialog();
+                        $("#childDialog").dialog("open");
+                    }
+                }
+            });
+
+            var weekdays = "td[class=mon], td[class=tue], td[class=wed], td[class=thu], td[class=fri], td[class=sat], td[class=sun]";
+            $(weekdays).on("click", function () {
+                $("#dialog").dialog("open");
+            });
+    }
+
+
+    createChildDialog();
+    createParentDialog();
+
 });
-
-$(document).ready(function() {
-    $( "#dialog" ).dialog({
-      autoOpen: false,
-      maxWidth:800,
-      maxHeight: 2000,
-      width: 500,
-      height: 700,
-      resizable: true,
-      stack: true,
-      show: {
-        effect: "blind",
-        duration: 800
-      },
-      hide: {
-        effect: "fade",
-        duration: 800
-      }
-    });
-
-    $( "td[class=mon], td[class=tue], td[class=wed]," +
-        "td[class=thu], td[class=fri], td[class=sat], td[class=sun]" ).on( "click", function() {
-      $( "#dialog" ).dialog( "open" );
-    });
-  });
-
-$( function() {
-    $( "#columnthree" ).selectable({
-        selected: function( event, ui ) {
-            console.log($(ui.selected).data('time'))
-        }});
-
-  } );
-//
-// $("#dialog").ready(function() {
-//     $( "#rowthree" ).dialog({
-//       autoOpen: false,
-//       maxWidth: 1000,
-//       maxHeight: 1000,
-//       width: 500,
-//       height: 250,
-//       resizable: true,
-//       show: {
-//         effect: "blind",
-//         duration: 800
-//       },
-//       hide: {
-//         effect: "fade",
-//         duration: 800
-//       }
-//     });
-//
-//     $( ".ui-selected" ).on( "click", function() {
-//       $( "#columnthree" ).dialog( "open" );
-//     });
-//   });
